42c34bba7fb99d97edc32ed11ecc76527296e1a2
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-04-08 09:04:24 +0430

    Colors enum added. version 0.1.0

---
+++ Colors.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * an {@code enum} of cards color
+ * 0: black, 1: red, 2: yellow, 3: green, 4: blue
+ * 
+ * @author Mohammad Mahdi Malmasi
+ * @version 0.1.0
+ */
+public enum Colors 
+{
+    BLACK,
+    RED,
+    YELLOW,
+    GREEN,
+    BLUE,
+}
