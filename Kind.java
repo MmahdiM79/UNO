c355acc27d1e8ceb7af710df18c9c8805067de2e
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-04-08 10:21:23 +0430

    Kind enum added. version 0.1.0

---
+++ Kind.java
@@ -0,0 +1,66 @@
+
+
+/**
+ * An {@code enum} of card kinds
+ * 
+ * @author Mohammad Mahdi Malmasi
+ * @version 0.1.0
+ */
+public enum Kind 
+{
+    RED_0,
+    YELLOW_0,
+    GREEN_0,
+    BLUE_0,
+
+    RED_1,
+    YELLOW_1,
+    GREEN_1,
+    BLUE_1,
+
+    RED_2,
+    YELLOW_2,
+    GREEN_2,
+    BLUE_2,
+
+    RED_3,
+    YELLOW_3,
+    GREEN_3,
+    BLUE_3,
+
+    RED_4,
+    YELLOW_4,
+    GREEEN_4,
+    BLUE_4,
+
+    RED_5,
+    YELLOW_5,
+    GREEEN_5,
+    BLUE_5,
+
+    RED_6,
+    YELLOW_6,
+    GREEN_6,
+    BLUE_6,
+
+    RED_7,
+    YELLOW_7,
+    GREEN_7,
+    BLUE_7,
+
+    RED_8,
+    YELLOW_8,
+    GREEN_8,
+    BLUE_8,
+
+    RED_9,
+    YELLOW_9,
+    GREEEN_9,
+    BLUE_9,
+
+    SKIP,
+    REVERSE,
+    DRAW2,
+    WILD_COLOR,
+    WILD_DRAW,
+}
