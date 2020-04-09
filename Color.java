d839d4b97222bf1f130a48837112938645b9d234
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-04-09 07:55:57 +0430

    file renamed. (Colors >>> Color)

---
+++ Color.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * an {@code enum} of card colors. 
+ * 0: black, 1: red, 2: yellow, 3: green, 4: blue
+ * 
+ * @author Mohammad Mahdi Malmasi
+ * @version 0.1.0
+ */
+public enum Color 
+{
+    BLACK,
+    RED,
+    YELLOW,
+    GREEN,
+    BLUE,
+}
