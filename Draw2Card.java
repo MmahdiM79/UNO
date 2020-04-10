0f470a66ac534f5947070f968fdee0a6c8975cd0
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-04-10 10:23:52 +0430

    minnor wrong comment fixed

--- Draw2Card.java
+++ Draw2Card.java
@@ -16,7 +16,7 @@ public class Draw2Card extends Card
            /* Constructor */
 
     /**
-     * Creat a new Draw2 card card with given details
+     * Creat a new Draw2 card with given details
      * 
      * @param cardColor : the color of the card
      * @param cardCode : the code number of the card. an int in range [1, 108]
