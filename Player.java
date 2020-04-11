9d84dfe22b9a02c57c7907bc0dedf2a4797a8374
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-04-11 09:16:36 +0430

    Cards and Password feilds and their methods added. (version 0.1.5)

--- Player.java
+++ Player.java
@@ -1,15 +1,17 @@
-
+import java.util.ArrayList;
 
 /**
  * This class repersent a player int Run class
- * it holds the player score, ID and player character
+ * it holds the player score, password and player cards
  * 
  * its extends from the person class
  * 
  * 
  * 
  * @author Mohammad Mahdi Malmasi
- * @version 0.1.0
+ * @version 0.1.5
+ * 
+ * @see Person
  */
 public class Player extends Person
 {
@@ -18,12 +20,11 @@ public class Player extends Person
     // the score of the player
     private int score;
 
-    // the id of the player in the game
-    private int playerID;
+    // the password of the player in the game
+    private String playerPass;
 
-    // the character of the player
-    private char playerCharacter;
-
+    // a list of player cards
+    private ArrayList<Card> playerCards;
 
 
 
@@ -32,13 +33,16 @@ public class Player extends Person
 
           /* Constructor */
 
-    public Player(String firstName, String lastName, int age, int playerID, char playerCharacter, String gender)
+    public Player(String firstName, String lastName, int age,  String playerPass, String gender)
     {
+        // set the super class
         super(firstName, lastName, age, gender);
 
+
         this.score = 0;
-        this.playerID = playerID;
-        this.playerCharacter = playerCharacter;
+        this.playerPass = playerPass;
+        
+        playerCards = new ArrayList<>();
     }
 
 
@@ -48,35 +52,6 @@ public class Player extends Person
 
 
             /* Methods */
-
-
-    // * setter methods *
-
-    /**
-     * set the score of the player
-     * @param score : the score of player to set
-     */
-    public void setScore(int score) 
-    {
-        this.score = score;
-    }
-    /**
-     * set the ID of the player
-     * @param playerID : the ID of palyer to set
-     */
-    public void setPlayerID(int playerID) 
-    {
-        this.playerID = playerID;
-    }
-    /**
-     * set the character of the player
-     * @param playerCharacter : the Character of player to set
-     */
-    public void setPlayerCharacter(char playerCharacter) 
-    {
-        this.playerCharacter = playerCharacter;
-    }
-
 
     // * getter methods *
 
@@ -88,17 +63,45 @@ public class Player extends Person
         return score;
     }
     /**
-     * @return : the ID of the player
+     * @return : the password of the player
      */
-    public int getPlayerID() 
+    public String getPlayerPass() 
     {
-        return playerID;
+        return playerPass;
     }
     /**
-     * @return : the Character of the player
+     * @return the player Cards
      */
-    public char getPlayerCharacter() 
+    public ArrayList<Card> getPlayerCards() 
     {
-        return playerCharacter;
+        return playerCards;
+    }
+
+
+
+
+
+    /**
+     * Add a new card to player cards
+     * 
+     * @param cardToAdd : new card to add
+     */
+    public void addCard(Card cardToAdd)
+    {
+        score += cardToAdd.getCardScore();
+        playerCards.add(cardToAdd);
+    }
+
+
+    /**
+     * Remove a card from player cards
+     * 
+     * @param cardToRemove
+     * @return {@code true} if and only if the player have the given card
+     */
+    public boolean removeCard(Card cardToRemove)
+    {
+        score -= cardToRemove.getCardScore();
+        return playerCards.remove(cardToRemove);
     }
 }
