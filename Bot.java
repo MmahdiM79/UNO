d675b666bd6323b1be26ba5508d0e926a2337f13
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-04-16 08:38:55 +0430

    Bot class added. version 0.0.0

---
+++ Bot.java
@@ -0,0 +1,19 @@
+
+
+
+/**
+ * @author Mohammad Mahdi Malmasi
+ * @version 0.0.0
+ */
+public class Bot extends Player
+{
+
+    /**
+     * Creat a new Bot 
+     * @param num : the number of the bot
+     */
+    public Bot(int num)
+    {
+        super("Bot"+num, ""+num);
+    }
+}
